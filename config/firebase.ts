993af4c292931f95ad1dcad8c5c16b8d8777ca13
00653000170 <00653000170@poupex.com.br>
2022-05-21 19:48:58 -0300

    incluindo firebase file

---
+++ config/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat";
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+}
+// Initialize Firebase
+export const appInitilizer = firebase.initializeApp(firebaseConfig);
+
+export const database = firebase.firestore()
+export const auth = firebase.auth()
+export const LOCAL_KEY = firebase.auth.Auth.Persistence.LOCAL
+
+
+
+
+
